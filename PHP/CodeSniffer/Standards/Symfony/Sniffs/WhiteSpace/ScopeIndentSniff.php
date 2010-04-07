aca643605b3940979a7b3fdad458fe6ec3674fd6
Dennis Benkert <spinecrasher@googlemail.com>
2010-04-07 21:49:12 +0200

    corrected the phpdoc comments of Symfony_Sniffs_Whitespace_ScopeIndentSniff

--- PHP/CodeSniffer/Standards/Symfony/Sniffs/WhiteSpace/ScopeIndentSniff.php
+++ PHP/CodeSniffer/Standards/Symfony/Sniffs/WhiteSpace/ScopeIndentSniff.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Generic_Sniffs_Whitespace_ScopeIndentSniff.
+ * Symfony_Sniffs_Whitespace_ScopeIndentSniff.
  *
  * PHP version 5
  *
@@ -8,14 +8,13 @@
  * @package   PHP_CodeSniffer
  * @author    Greg Sherwood <gsherwood@squiz.net>
  * @author    Marc McIntyre <mmcintyre@squiz.net>
- * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
- * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @author    Dennis Benkert <spinecrasher@googlemail.com>
  * @version   CVS: $Id: ScopeIndentSniff.php 270281 2008-12-02 02:38:34Z squiz $
  * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
 
 /**
- * Generic_Sniffs_Whitespace_ScopeIndentSniff.
+ * Symfony_Sniffs_Whitespace_ScopeIndentSniff.
  *
  * Checks that control structures are structured correctly, and their content
  * is indented correctly. This sniff will throw errors if tabs are used
@@ -25,9 +24,7 @@
  * @package   PHP_CodeSniffer
  * @author    Greg Sherwood <gsherwood@squiz.net>
  * @author    Marc McIntyre <mmcintyre@squiz.net>
- * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
- * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
- * @version   Release: 1.2.2
+ * @author    Dennis Benkert <spinecrasher@googlemail.com>
  * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
 class Symfony_Sniffs_WhiteSpace_ScopeIndentSniff extends Generic_Sniffs_WhiteSpace_ScopeIndentSniff
